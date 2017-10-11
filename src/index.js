494d7194e0162f4b5fdc190cf00162602e52ac5b
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2017-10-10 20:39:44 -0400

    set up db connection

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@
 // load modules
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/dev');
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.error(`connection error: ${err}`);
+});
+
+db.once("open", function () {
+  console.log(`db connection successful!`);
+});
 
 // set our port
 app.set('port', process.env.PORT || 5000);
@@ -16,14 +29,14 @@ app.use(morgan('dev'));
 app.use('/', express.static('public'));
 
 // catch 404 and forward to global error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
 
 // Express's global error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -32,6 +45,6 @@ app.use(function(err, req, res, next) {
 });
 
 // start listening on our port
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
   console.log('Express server is listening on port ' + server.address().port);
 });
