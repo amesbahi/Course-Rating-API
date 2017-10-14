bc470e8a9f29cff5b08b9d7b42539fb79ff58d2b
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2017-10-14 13:15:52 -0400

    create user routes

---
+++ src/middleware/index.js
@@ -0,0 +1,6 @@
+function requiresLogin(req, res, next) {
+    if (req.session && req.session.userId) {
+        return res.redirect('/profile');
+    }
+    return next();
+}
---
+++ src/routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var User = require('./models');
+var Course = require('./models');
+var Review = require('./models');
+var mid = require('../middleware');
+
+/****** USER ROUTES ******/
+
+// Return auth user
+router.get('/users', mid.requiresLogin, function (req, res, next) {
+    User.findById(req.session.userId)
+        .exec(function (error, user) {
+            if (error) {
+                return next(error)
+            } else {
+                res.status(200);
+                return res.render('profile', { title: 'Profile', name: user.fullName });
+            }
+        });
+});
+
+// Create user
+router.post('/users', function (req, res, next) {
+    if (req.body.fullName &&
+        req.body.emailAddress &&
+        req.body.password) {
+
+        // object with form input
+        var userData = {
+            fullName: req.body.fullName,
+            emailAddress: req.body.emailAddress,
+            password: req.body.password
+        };
+
+        // schema's 'create' method to insert document into Mongo
+        User.create(userData, function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                req.session.userId = user._id;
+                // set location header to '/', return no content
+                res.location('/');
+                res.status(201);
+                return res.redirect('/profile');
+            }
+        });
+
+    } else {
+        var err = new Error('All fields required.');
+        err.status = 400;
+        return next(err);
+    }
+});
